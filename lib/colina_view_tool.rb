e59b0788826e6e305282eecb61b96665715cb412
gerardo colina <gerardocolina@gmail.com>
2017-09-25 00:19:35 -0400

    Dev tool initial commit

---
+++ lib/colina_view_tool.rb
@@ -0,0 +1,6 @@
+require "colina_view_tool/version"
+require "colina_view_tool/renderer"
+
+module ColinaViewTool
+  # Your code goes here...
+end
---
+++ lib/colina_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ColinaViewTool
+  VERSION = "0.1.0"
+end
